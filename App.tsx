43b8611fb3a6dab77ad03c3cb8bb130e1a4aedb1
Raisson Souza <raissonrai@gmail.com>
2024-10-11 14:40:49 -0300

    - Implementado StatusBar; - Refatorado estilo das navegações.

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer"
 import { createStackNavigator } from "@react-navigation/stack"
 import { Home } from "./src/screens/Home"
 import { NavigationContainer } from "@react-navigation/native"
+import { StatusBar } from "expo-status-bar"
 import Icon from "react-native-vector-icons/Ionicons"
 import React from "react"
 
@@ -14,10 +15,30 @@ export type TabNavigationParams = {
 
 const Tab = createBottomTabNavigator<TabNavigationParams>()
 
+const TabScreenStyle = {
+  /** Cor de fundo da tab ativa */
+  tabBarActiveBackgroundColor: "",
+  /** Cor de fundo das tabs inativas */
+  tabBarInactiveBackgroundColor: "",
+  /** Cor de título do header da tab atual */
+  headerTintColor: "white",
+  /** Cor de fundo do header da tab atual */
+  headerStyle: {
+    backgroundColor: "darkblue"
+  },
+  /** Cor de fundo do footer */
+  tabBarStyle: {
+    backgroundColor: "darkblue"
+  }
+}
+
 /** Componente de navegação por tab */
 const TabNavigator = () => {
   return (
-    <Tab.Navigator initialRouteName="Home">
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={ TabScreenStyle }
+    >
       <Tab.Screen
         name="Home"
         component={ Home }
@@ -39,10 +60,20 @@ export type StackNavigationParams = {
 
 const Stack = createStackNavigator<StackNavigationParams>()
 
+const StackScreenStyle = {
+  /** Cor do header */
+  headerStyle: { backgroundColor: "darkblue" },
+  /** Cor do título do header */
+  headerTintColor: "white",
+}
+
 /** Componente de navegação por stack */
 const StackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Tabs">
+    <Stack.Navigator
+      initialRouteName="Tabs"
+      screenOptions={ StackScreenStyle }
+    >
       <Stack.Screen
         name="Tabs"
         component={ TabNavigator }
@@ -59,14 +90,36 @@ export type DrawerNavigationParams = {
 
 const Drawer = createDrawerNavigator<DrawerNavigationParams>()
 
+const drawerScreensStyle = {
+  /** Cor do ícone do drawer e título */
+  headerTintColor: "white",
+  /** Cor do título da aba selecionada na section do drawer */
+  drawerActiveTintColor: "white",
+  /** Cor dos títulos das abas não selecionadas na section do drawer */
+  drawerInactiveTintColor: "grey",
+  /** Cor do header */
+  headerStyle: { backgroundColor: "darkblue" }
+}
+
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Stack">
+      <StatusBar
+        // backgroundColor="#"
+      />
+      <Drawer.Navigator
+        initialRouteName="Stack"
+        screenOptions={{
+          drawerStyle:{
+            /** Cor de fundo da section do drawer */
+            backgroundColor: "darkblue"
+          }
+        }}
+      >
         <Drawer.Screen
           name="Stack"
           component={ StackNavigator }
-          options={{ title: "Home" }}
+          options={{ ...drawerScreensStyle, title: "Home" }}
         />
       </Drawer.Navigator>
     </NavigationContainer>
