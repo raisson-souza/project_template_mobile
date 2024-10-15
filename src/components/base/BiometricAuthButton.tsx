fe407aa15398a70123409060432959e4764580c7
Raisson Souza <raissonrai@gmail.com>
2024-10-15 16:19:26 -0300

    Implementado botão customizado para autenticação via biometria.

---
+++ src/components/base/BiometricAuthButton.tsx
@@ -0,0 +1,60 @@
+import { Button } from "react-native"
+import * as LocalAuthentication from 'expo-local-authentication'
+import React from "react"
+
+type BiometricAuthButtonProps = {
+    /** Mensagem da solicitação de biometria */
+    authMessage?: string
+    /** Mensagem do botão para cancelar biometria */
+    cancelAuthTitle?: string
+    /** Título do botão PROP TEMPORÁRIA */
+    btnTitle?: string
+    /** Ação após sucesso */
+    onSuccess?: () => any
+    /** Ação após falha */
+    onFailure?: () => any
+    /** State indicando suceso na autenticação */
+    authSuccess: boolean
+    /** Set State do sucesso na autenticação */
+    setAuthSuccess: React.Dispatch<React.SetStateAction<boolean>>
+    /** Propriedade especial para as props do botão customizado */
+    btnProps?: never
+}
+
+export default function BiometricAuthButton({
+    authMessage = "Confirmar ação",
+    cancelAuthTitle = "Cancelar",
+    btnTitle = "Confirmar",
+    onSuccess = () => {},
+    onFailure = () => {},
+    authSuccess,
+    setAuthSuccess,
+}: BiometricAuthButtonProps): JSX.Element {
+    const onPress = async () => {
+        if (authSuccess) return
+
+        const auth = await LocalAuthentication.authenticateAsync({
+            promptMessage: authMessage,
+            cancelLabel: cancelAuthTitle,
+            biometricsSecurityLevel: "strong",
+            disableDeviceFallback: true,
+        })
+
+        if (auth.success) {
+            setAuthSuccess(true)
+            await onSuccess()
+            return
+        }
+
+        await onFailure()
+    }
+
+    // TODO: Trocar botão pelo botão customizado
+    return (
+        <Button
+            title={ btnTitle }
+            disabled={ authSuccess }
+            onPress={ onPress }
+        />
+    )
+}
