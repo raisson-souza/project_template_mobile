52c4f418051fd93c53f733aadcd8b4cf5f98101a
Raisson Souza <raissonrai@gmail.com>
2024-10-17 16:09:55 -0300

    - Remoção de código teste.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,9 +3,8 @@ import { RouteProp, useNavigation, useRoute } from "@react-navigation/native"
 import { Screen } from "../components/base/Screen"
 import { StackNavigationParams, TabNavigationParams } from "../../App"
 import { StackNavigationProp } from "@react-navigation/stack"
-import { Text, StyleSheet, Image } from "react-native"
-import React, { useState } from "react"
-import CameraPicker, { ImagePickerImageProps } from "../components/CameraPicker"
+import { Text, StyleSheet } from "react-native"
+import React from "react"
 
 type HomeStackUseNavigationProps = StackNavigationProp<StackNavigationParams, "Tabs">
 type HomeStackUseRouteProps = RouteProp<StackNavigationParams, "Tabs">
@@ -18,16 +17,10 @@ export const Home: React.FC<{}> = ({ }) => {
     const tabNavigation = useNavigation<HomeTabUseNavigationProps>()
     const stackRoute = useRoute<HomeStackUseRouteProps>()
     const tabRoute = useRoute<HomeTabUseRouteProps>()
-    const [ images, setImages ] = useState<ImagePickerImageProps[]>([])
-
-    console.log("images len", images.length)
-    console.log("image 0 uri", images[0].uri)
 
     return (
         <Screen>
             <Text>Project Template Mobile</Text>
-            <CameraPicker images={ images } setImages={ setImages } />
-            { images.length > 0 ? <Image alt="aa" source={{ uri: images[0].uri }} /> : <></> }
         </Screen>
     )
 }
