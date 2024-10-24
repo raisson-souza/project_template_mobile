60ca3d1a223d8cf1d210f3a6696c2b7421e51c95
Raisson Souza <raissonrai@gmail.com>
2024-10-24 15:27:46 -0300

    Implementado NotificationSender para criação de notificações personalizadas.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,6 +7,7 @@ import { Text, StyleSheet, View, Button, Platform } from "react-native"
 import { useState, useEffect, useRef } from 'react'
 import * as Notifications from 'expo-notifications'
 import React from "react"
+import NotificationSender from "../utils/NotificationSender"
 
 type HomeStackUseNavigationProps = StackNavigationProp<StackNavigationParams, "Tabs">
 type HomeStackUseRouteProps = RouteProp<StackNavigationParams, "Tabs">
@@ -14,49 +15,11 @@ type HomeStackUseRouteProps = RouteProp<StackNavigationParams, "Tabs">
 type HomeTabUseNavigationProps = BottomTabNavigationProp<TabNavigationParams, "Home">
 type HomeTabUseRouteProps = RouteProp<TabNavigationParams, "Home">
 
-Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-        shouldShowAlert: true,
-        shouldPlaySound: false,
-        shouldSetBadge: false,
-    }),
-})
-
-async function schedulePushNotification() {
-    await Notifications.scheduleNotificationAsync({
-        content: {
-            title: "You've got mail! 📬",
-            body: 'Here is the notification body',
-            data: { data: 'goes here', test: { test1: 'more data' } },
-        },
-        trigger: { seconds: 2 },
-    })
-}
-
 export const Home: React.FC<{}> = ({ }) => {
     const stackNavigation = useNavigation<HomeStackUseNavigationProps>()
     const tabNavigation = useNavigation<HomeTabUseNavigationProps>()
     const stackRoute = useRoute<HomeStackUseRouteProps>()
     const tabRoute = useRoute<HomeTabUseRouteProps>()
-
-    const [notification, setNotification] = useState<Notifications.Notification | undefined>(undefined)
-    const notificationListener = useRef<Notifications.Subscription>()
-    const responseListener = useRef<Notifications.Subscription>()
-
-    useEffect(() => {
-        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-            setNotification(notification)
-        })
-
-        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-            console.log(response)
-        })
-
-        return () => {
-            notificationListener.current && Notifications.removeNotificationSubscription(notificationListener.current);
-            responseListener.current && Notifications.removeNotificationSubscription(responseListener.current);
-        }
-    }, [])
 
     return (
         <Screen>
@@ -67,15 +30,10 @@ export const Home: React.FC<{}> = ({ }) => {
                     justifyContent: 'space-around',
                 }}
             >
-                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                    <Text>Title: {notification && notification.request.content.title} </Text>
-                    <Text>Body: {notification && notification.request.content.body}</Text>
-                    <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-                </View>
                 <Button
-                    title="Press to schedule a notification"
+                    title="ENVIAR NOTIFICAÇÃO"
                     onPress={async () => {
-                        await schedulePushNotification();
+                        await NotificationSender({ title: "Cagaro" })
                     }}
                 />
             </View>
---
+++ src/utils/NotificationSender.ts
@@ -0,0 +1,37 @@
+import * as Notifications from 'expo-notifications'
+
+type NotificationSenderProps = {
+    title: string
+    body?: string
+    showAlert?: boolean
+    playSound?: boolean
+    awaitSeconds?: number
+}
+
+export default async function NotificationSender({
+    title,
+    body = "Verifique o aplicativo.",
+    showAlert = true,
+    playSound = true,
+    awaitSeconds = 1,
+}: NotificationSenderProps) {
+    Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+            shouldShowAlert: showAlert,
+            shouldPlaySound: playSound,
+            shouldSetBadge: false,
+            priority: Notifications.AndroidNotificationPriority.MAX
+        }),
+    })
+
+    await Notifications.scheduleNotificationAsync({
+        content: { // TODO: verificar outras props
+            title: title,
+            body: body,
+        },
+        trigger: {
+            seconds: awaitSeconds,
+            channelId: 'default',
+        },
+    })
+}
