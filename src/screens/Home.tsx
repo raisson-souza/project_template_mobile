30999bca8ec038ae20f42da6dab5bdfb20ad099d
Raisson Souza <raissonrai@gmail.com>
2024-10-24 14:43:18 -0300

    - Pré implementação de notificações.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,7 +3,11 @@ import { RouteProp, useNavigation, useRoute } from "@react-navigation/native"
 import { Screen } from "../components/base/Screen"
 import { StackNavigationParams, TabNavigationParams } from "../../App"
 import { StackNavigationProp } from "@react-navigation/stack"
-import { Text, StyleSheet } from "react-native"
+import { Text, StyleSheet, View, Button, Platform } from "react-native"
+import { useState, useEffect, useRef } from 'react'
+import * as Device from 'expo-device'
+import * as Notifications from 'expo-notifications'
+import Constants from 'expo-constants'
 import React from "react"
 
 type HomeStackUseNavigationProps = StackNavigationProp<StackNavigationParams, "Tabs">
@@ -12,15 +16,124 @@ type HomeStackUseRouteProps = RouteProp<StackNavigationParams, "Tabs">
 type HomeTabUseNavigationProps = BottomTabNavigationProp<TabNavigationParams, "Home">
 type HomeTabUseRouteProps = RouteProp<TabNavigationParams, "Home">
 
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+})
+
+async function schedulePushNotification() {
+    await Notifications.scheduleNotificationAsync({
+        content: {
+            title: "You've got mail! 📬",
+            body: 'Here is the notification body',
+            data: { data: 'goes here', test: { test1: 'more data' } },
+        },
+        trigger: { seconds: 2 },
+    })
+}
+
+async function registerForPushNotificationsAsync() {
+    let token
+
+    if (Platform.OS === 'android') {
+        await Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        })
+    }
+
+    if (Device.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync()
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync()
+            finalStatus = status
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!')
+            return
+        }
+
+        try {
+            const projectId = Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+            if (!projectId)
+                throw new Error('Project ID not found')
+
+            token = (
+                await Notifications.getExpoPushTokenAsync({
+                    projectId,
+                })
+            ).data
+            console.log(token)
+        } catch (e) {
+            token = `${e}`
+        }
+    } else
+        alert('Must use physical device for Push Notifications')
+
+    return token
+}
+
+
 export const Home: React.FC<{}> = ({ }) => {
     const stackNavigation = useNavigation<HomeStackUseNavigationProps>()
     const tabNavigation = useNavigation<HomeTabUseNavigationProps>()
     const stackRoute = useRoute<HomeStackUseRouteProps>()
     const tabRoute = useRoute<HomeTabUseRouteProps>()
 
+    const [expoPushToken, setExpoPushToken] = useState('')
+    const [channels, setChannels] = useState<Notifications.NotificationChannel[]>([])
+    const [notification, setNotification] = useState<Notifications.Notification | undefined>(undefined)
+    const notificationListener = useRef<Notifications.Subscription>()
+    const responseListener = useRef<Notifications.Subscription>()
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => token && setExpoPushToken(token))
+
+        if (Platform.OS === 'android')
+            Notifications.getNotificationChannelsAsync().then(value => setChannels(value ?? []))
+
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification)
+        })
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response)
+        })
+
+        return () => {
+            notificationListener.current && Notifications.removeNotificationSubscription(notificationListener.current);
+            responseListener.current && Notifications.removeNotificationSubscription(responseListener.current);
+        }
+    }, [])
+
     return (
         <Screen>
-            <Text>Project Template Mobile</Text>
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'space-around',
+                }}
+            >
+                <Text>Your expo push token: {expoPushToken}</Text>
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>Title: {notification && notification.request.content.title} </Text>
+                    <Text>Body: {notification && notification.request.content.body}</Text>
+                    <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+                </View>
+                <Button
+                    title="Press to schedule a notification"
+                    onPress={async () => {
+                        await schedulePushNotification();
+                    }}
+                />
+            </View>
         </Screen>
     )
 }
