055ba3f52d18a7e0ef2f2bdef3f9f0e50f19a215
Raisson Souza <raissonrai@gmail.com>
2024-10-24 15:43:48 -0300

    Implementadas novas propriedades de notificação em NotificationSender.

--- src/utils/NotificationSender.ts
+++ src/utils/NotificationSender.ts
@@ -2,18 +2,22 @@ import * as Notifications from 'expo-notifications'
 
 type NotificationSenderProps = {
     title: string
+    subtitle?: string
     body?: string
     showAlert?: boolean
     playSound?: boolean
     awaitSeconds?: number
+    appLogoBackgroundColor?: string
 }
 
 export default async function NotificationSender({
     title,
+    subtitle = undefined,
     body = "Verifique o aplicativo.",
     showAlert = true,
     playSound = true,
     awaitSeconds = 1,
+    appLogoBackgroundColor = "#00008B",
 }: NotificationSenderProps) {
     Notifications.setNotificationHandler({
         handleNotification: async () => ({
@@ -28,7 +32,9 @@ export default async function NotificationSender({
         content: { // TODO: verificar outras props
             title: title,
             body: body,
-            data: {} // Configurar dados da notificação aqui caso seja necessário implementar captura destes dados
+            data: {}, // Configurar dados da notificação aqui caso seja necessário implementar captura destes dados
+            color: appLogoBackgroundColor,
+            subtitle: subtitle,
         },
         trigger: {
             seconds: awaitSeconds,
