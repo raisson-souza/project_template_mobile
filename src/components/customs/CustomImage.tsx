4b55538eb34e028617d60282390a956951942306
Raisson Souza <raissonrai@gmail.com>
2024-11-11 15:58:35 -0300

    Reimplementação do CustomImage para suporte a imagens locais e online.

--- src/components/customs/CustomImage.tsx
+++ src/components/customs/CustomImage.tsx
@@ -1,42 +1,57 @@
-import { ImagePickerImageProps } from "./CameraPicker"
-import { Text, Image, ImageProps } from "react-native"
+import { Image } from "react-native"
 
-export type CustomImageProps = {
-    /** Propriedades da imagem */
-    imageProps?: ImagePickerImageProps | null
-    /** Referência da imagem */
-    alt: string
-    /** Exibir referência quando não houver imagem */
-    showAlt?: boolean
-    width?: number
-    height?: number
-    /** Propriedades do Image (irão sobrepor as outras) */
-    innerProps?: ImageProps
+type CustomImageTypes = {
+    Local: (props: LocalCustomImageProps) => JSX.Element
+    Online: (props: OnlineCustomImageProps) => JSX.Element
 }
 
-/** Componente customizado para exibição de imagens */
-export default function CustomImage({
-    imageProps,
-    alt,
-    showAlt = true,
-    width = 200,
-    height = 200,
-    innerProps = {},
-}: CustomImageProps) {
-    if (imageProps) {
+/** Tipagem para custom image de imagens locais */
+type LocalCustomImageProps = {
+    /**
+     * Uso da função require para retornar o caminho da imagem
+     * @example require("./app_logo.png")
+     * */
+    filePathByRequire: any
+} & CustomImageProps
+
+/** Tipagem para custom image de imagens online */
+type OnlineCustomImageProps = {
+    /**
+     * URL da imagem online
+     * @example url="https://site/image.png"
+     * */
+    url: string
+} & CustomImageProps
+
+type CustomImageProps = {
+    width?: number
+    height?: number
+}
+
+/**
+ * Componente customizado para exibição de imagens locais ou online
+ * @example <CustomImage.Local filePathByRequire={ require("../assets/app_logo.png") }
+ * @example <CustomImage.Online url="https://site/image.png"
+ * */
+export const CustomImage: CustomImageTypes = {
+    Local: ({ filePathByRequire, height = 200, width = 200 }: LocalCustomImageProps) => {
         return <Image
-            alt="imagem"
-            source={{ uri: imageProps.uri }}
+            alt="custom_local_image"
+            source={ filePathByRequire }
             style={{
                 width: width,
                 height: height,
             }}
-            { ...innerProps }
         />
-    }
-
-    if (showAlt)
-        return <Text>{ alt }</Text>
-
-    return <></>
+    },
+    Online: ({ url, height = 200, width = 200 }: OnlineCustomImageProps) => {
+        return <Image
+            alt="custom_online_image"
+            source={{ uri: url }}
+            style={{
+                width: width,
+                height: height,
+            }}
+        />
+    },
 }
